b42d2fe1a17af91f531de794a4acfefdcfbecc9a
Dipti Tawari <dipti_tiwari@naxgrp.com>
2023-09-19 19:13:28 +0530

    changes done to create new template.

--- content/sonar.sh
+++ content/sonar.sh
@@ -1,5 +1,5 @@
 
 #!/bin/sh
 mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
--Dsonar.projectKey= ${{ values.projectKey | dump }}
--Dsonar.projectName=${{values.artifact_id}}
+-Dsonar.projectKey= ${{ values.projectKey }}
+-Dsonar.projectName=${{ values.projectName }}
