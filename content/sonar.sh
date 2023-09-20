675e1eb46c0b4b9982728479902843e14e26e8b0
Dipti Tawari <dipti_tiwari@naxgrp.com>
2023-09-20 17:36:28 +0530

    sonarqube to sonar cloud

--- content/sonar.sh
+++ content/sonar.sh
@@ -1,5 +1,3 @@
 
 #!/bin/sh
-mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
--Dsonar.projectKey= ${{ values.projectKey }}
--Dsonar.projectName=${{ values.projectName }}
+mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=${{ values.projectKey }}
